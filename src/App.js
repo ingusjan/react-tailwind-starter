7eb533fe3fc79a2de7f61e464a126b7cf2f6941a
Ingus <ingusjan@gmail.com>
2020-12-09 21:24:29 +0000

    ✨ Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="pt-12 w-screen h-screen bg-gray-100 text-center">
+      <h1 className="text-4xl font-sans mb-4">React + Tailwind = Awesome</h1>
+      <p>Forget about writing CSS files... It's all about CSS-in-JS now</p>
+      <img
+        class="m-auto mt-12"
+        src="https://media1.tenor.com/images/6461310eeb6751d0537b6951dbc15af3/tenor.gif"
+        alt="Surprised Cat Gif"
+      />
+    </div>
+  );
+}
+
+export default App;
